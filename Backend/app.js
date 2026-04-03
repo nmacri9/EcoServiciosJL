75ff54ee9f2dc522150a63e52fd9665c8be84880
nmacri9 <nicolasmacri09@gmail.com>
2026-04-02 21:13:53 -0300

    mas cambioss

--- Backend/app.js
+++ Backend/app.js
@@ -31,7 +31,7 @@ app.get('/servicios', (req, res) => {
         UNION
         SELECT id, nombre, descripcion, imagen, precio, 'Seguridad e Higiene' as categoria 
         FROM seguridad_higiene_y_medioambiente
-        SELECT id, nombre, descripcion, imagen, precio, 'Seguridad e Higiene' as categoria 
+        SELECT id, nombre, descripcion, imagen, precio, 'Gestión y Trámites' as categoria 
         FROM gestiones
 
     `;
